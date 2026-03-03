cce7eb67d06d3a1fb2c17caeb5bef3e753efdc9a
horaoen <horaoen@gmail.com>
2026-03-03 10:57:08 +0800

    update

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,11 +1,15 @@
 return {
-  "nvim-java/nvim-java",
-  config = function()
-    require("java").setup()
-    vim.lsp.enable("jdtls")
-  end,
-  ft = { "java" },
-  keys = {
-    { "<leader>fj", "<cmd>:FzfLua lsp_live_workspace_symbols<cr>", desc = "Workspace Symbols Search" },
+  {
+    "nvim-java/nvim-java",
+    ft = { "java", "yaml", "jproperties" },
+    config = function()
+      require("java").setup({
+        spring_boot_tools = { enable = true },
+      })
+      vim.lsp.enable("jdtls")
+    end,
+    keys = {
+      { "<leader>fj", "<cmd>:FzfLua lsp_live_workspace_symbols<cr>", desc = "Workspace Symbols Search" },
+    },
   },
 }
